8e4250b1311fbe14fe439dfe15f4690dffa4c56d
Alexander Kiselyov <aleksandr.kiselyov@gmail.com>
2020-01-31 00:58:41 +0300

    Added some kinetic energy stats

--- nbody_sim/plot.py
+++ nbody_sim/plot.py
@@ -65,6 +65,14 @@ def get_body_energies(body_info):
     return 0.5 * body_info['m'] * (body_info['vx']**2 + body_info['vy']**2 + body_info['vz']**2)
 
 
+def _get_energy_stats(energies):
+    return {
+        'max_diff': np.abs(energies - energies[0]).max() / energies[0],
+        'rel_stdev': energies.std() / energies[0],
+        'avg': energies.mean()
+    }
+
+
 def plot_energy(run_name : str, save_path : Optional[Path], body_list : Optional[List[str]], is_cumulative : bool) -> None:
     body_infos = load_simulation_data(run_name)
 
@@ -77,10 +85,16 @@ def plot_energy(run_name : str, save_path : Optional[Path], body_list : Optional
         body_times = _get_times_from_body_infos(body_infos)
         cumulative_energy = sum(body_energies.values())
         plt.plot(body_times, cumulative_energy, label='Cumulative')
+
+        print('--- Cumulative energy stats ---')
+        print('avg {avg:.5e}, relative stdev {rel_stdev:.3}, max diff {max_diff:.3}'.format(**_get_energy_stats(cumulative_energy)))
     else:
+        print('--- Per body energy stats ---')
         for body_name in body_list:
             body_info = body_infos[body_name]
             plt.plot(body_info['t'], body_energies[body_name], label=body_name)
+            print('{body_name}: avg {avg:.5e}, relative stdev {rel_stdev:.3}, max diff {max_diff:.3}'\
+                  .format(body_name=body_name, **_get_energy_stats(body_energies[body_name])))
 
     plt.ylabel('Energy, J')
     plt.xlabel('Time, s')
